c47f751e8b7e09e5904779d01cc677b6585f5cf2
Oliver Gao <gmhpwx@gmail.com>
2022-10-19 09:11:39 +1100

    Improve

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <section class="section"><h1>This is the home page.</h1></section>
+  <section class="section">
+    <h1>This is the home page.</h1>
+    <section class="section tips">
+      <ul></ul>
+    </section>
+  </section>
 </template>
